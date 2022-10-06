5726d40144df7885b32ef6fb69a312e82bd32abe
Ahkal <bp@sojourn.llc>
2022-10-05 23:50:55 -0400

    notes for learning

---
+++ dataTypes.py
@@ -0,0 +1,18 @@
+# Numbers:
+# --Integers
+# For whole numbers only.
+# Can be positive/negative.
+# No decimal points.
+print(31_000_000_000)
+# |
+# |
+# --Floats
+# Written w/ decimal point.
+# Can be positive/negative.
+print(.7e-3)
+
+# Variables
+retire = 35
+age = 30
+difference = retire - age
+print(difference, 'years till retirement, we NEED to grind!')
---
+++ operators.py
@@ -0,0 +1,21 @@
+# Addition (+)
+# If there's a float involved, the answer will be a float.
+print(.5 + 5.4 + 2)
+
+# Subtraction (-)
+print(1 - 4)
+
+# Division
+print(4 / 2)
+
+# Multiplication
+print(3 * 7)
+
+# Exponents
+print(3**2)
+
+# Integer Division - Rounds down
+print(3//4)
+
+# Modulo - Remainder
+print(10 % 3)
