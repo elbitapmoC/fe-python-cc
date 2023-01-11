3d15955931261b4c8570ea7b5d01535e84ed1416
Ahkal <bp@sojourn.llc>
2023-01-11 11:53:59 -0500

    Adds in numeric notation

---
+++ numericNotations.py
@@ -0,0 +1,12 @@
+# Integers can NOT have a leading 0
+print(01)  # Will not work.
+
+# Floats can have a leading 0.
+print(0.333)  # will work.
+
+# For readability add _ to the numbers
+print(1_234_567_891)
+# ^ Read as 1,234,567,891
+
+# Floats may have scientific notation
+print(7e-3)
