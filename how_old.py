4bdde0ed0047f698ae1da1fad6fc75b05a7e7320
Ahkal <bp@sojourn.llc>
2023-01-11 17:18:49 -0500

    game to check age in days

---
+++ how_old.py
@@ -0,0 +1,8 @@
+age = input('How old are you?')
+30
+
+# OLD
+# print('You are, ' + str((int(age) * 365)) + ' days old.')
+
+# NEW
+print(f"You are, {int(age) * 365} days old.")
---
+++ use_functions.py
@@ -0,0 +1,17 @@
+# Length
+print(len('strings have a size, the index starts at 0'))
+
+# input - Always gives us a strings.
+input("How old are you?")
+
+age = '12'
+time = 12
+
+# convert to int
+print(int(age))
+
+# convert to float
+print(int(age))
+
+# convert to string
+print(str(time))
