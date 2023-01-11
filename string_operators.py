e3e744925511b4c8b7533a652d08c64d67a80366
Ahkal <bp@sojourn.llc>
2023-01-11 14:30:38 -0500

    Indexing strings and using None type

---
+++ string_operators.py
@@ -0,0 +1,15 @@
+fName = 'Corn'
+lName = 'Wallace'
+full_name = fName + " " + lName
+print(full_name)
+
+print(fName * 3)
+print(3 * lName)
+
+print("Hellooooo Nurse!"[0])  # output: H
+print(fName[1])  # output: l
+print(lName[3])  # output: l
+print(full_name[-1])  # output: e
+
+# None is same as null in js.
+print(type(None))  # NoneType
