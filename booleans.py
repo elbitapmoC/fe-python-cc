0ee326c29a3d71ae31cbddcb9ed183a7aa10d91c
Ahkal <bp@sojourn.llc>
2023-01-12 22:03:00 -0500

    booleans, true or false

---
+++ booleans.py
@@ -0,0 +1,27 @@
+age = 20
+if age >= 21:
+    print('You can enter')
+else:
+    print('Come back later...')
+# >, greater than
+
+# >=, greater than or equal to
+
+# <, less than
+
+# <=, less than or equal to
+
+# ==, equal to
+
+# !=, not equal to
+
+# These evaluate to FALSE!
+# False
+# 0
+# 0.0
+# ""
+# """ """
+# None
+# []
+# {}
+# ()
